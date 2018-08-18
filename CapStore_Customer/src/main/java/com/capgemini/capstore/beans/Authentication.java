ab7e8eb5a3d721ce283b533a1e5e4e8ddc871e38
ADM-IG-HWDLAB1C <ADM-IG-HWDLAB1C@PUNHWD13074.corp.capgemini.com>
2018-08-18 19:59:12 +0530

    Register and Search functionalities are added

--- CapStore_Customer/src/main/java/com/capgemini/capstore/beans/Authentication.java
+++ CapStore_Customer/src/main/java/com/capgemini/capstore/beans/Authentication.java
@@ -11,7 +11,6 @@ import javax.persistence.Table;
 public class Authentication {
 
 	@Id
-	@GeneratedValue(strategy=GenerationType.AUTO)
 	private int authId;
 	private String mobileNo;
 	private String password;
