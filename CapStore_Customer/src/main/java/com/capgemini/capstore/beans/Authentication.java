7e3728e47cd59b7edd0e47449f30d64d55c7d4cf
Ashritha Setlur <ashritha.setlur@capgemini.com>
2018-08-19 17:49:00 +0530

    Devtools And Jackson Added

--- CapStore_Customer/src/main/java/com/capgemini/capstore/beans/Authentication.java
+++ CapStore_Customer/src/main/java/com/capgemini/capstore/beans/Authentication.java
@@ -1,8 +1,6 @@
 package com.capgemini.capstore.beans;
 
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
