04b72a7cc7a69bb5fcb13a9f7627bc5298e66c4d
Ashritha Setlur <ashritha.setlur@capgemini.com>
2018-08-19 16:51:12 +0530

    Chnages Made in CustomerUI

--- CapStore_Customer/src/main/java/com/capgemini/capstore/controllers/CustomerUIController.java
+++ CapStore_Customer/src/main/java/com/capgemini/capstore/controllers/CustomerUIController.java
@@ -1,28 +1,16 @@
 package com.capgemini.capstore.controllers;
 
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-import org.json.JSONException;
-import org.json.JSONObject;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
-import com.capgemini.capstore.beans.Address;
-import com.capgemini.capstore.beans.Authentication;
-import com.capgemini.capstore.beans.Customer;
-import com.capgemini.capstore.beans.Merchant;
 import com.capgemini.capstore.beans.Product;
-import com.capgemini.capstore.services.MerchantServices;
 
 @Controller
 @RestController
--- CapStore_DB/src/main/java/com/capgemini/capstore/services/MerchantServicesImpl.java
+++ CapStore_DB/src/main/java/com/capgemini/capstore/services/MerchantServicesImpl.java
@@ -1,11 +1,15 @@
 package com.capgemini.capstore.services;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import com.capgemini.capstore.beans.Authentication;
+import com.capgemini.capstore.beans.Customer;
 import com.capgemini.capstore.beans.Merchant;
+import com.capgemini.capstore.beans.Product;
 import com.capgemini.capstore.repo.MerchantRepo;
 @Component
 public class MerchantServicesImpl implements MerchantServices {
@@ -33,6 +37,39 @@ public class MerchantServicesImpl implements MerchantServices {
 	@Override
 	public void deleteMerchant(int merchantId) {
 		mrepo.deleteById(merchantId);
+	}
+	@Override
+	public Customer registerCustomer(Customer customer,Authentication passwrd) {
+		
+		Customer customer1;
+		crepo.save(customer);
+		customer1=crepo.getCustomerId(customer.getMobileNo());
+	    //BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+		String password=passwrd.getPassword();
+        Authentication auth=new Authentication(customer1.getId(),customer1.getMobileNo(),password,"USER_CUSTOMER",2);
+        arepo.save(auth);
+	  return customer;
+	}
+	
+	//Searching product by Brand
+	@Override
+	public ArrayList<Product> searchByProductBrand(String product) {
+		ArrayList<Product> product1=new ArrayList<Product>();
+	
+		product1=prepo.findProductBrand(product);
+		if(product1==null)
+		{
+			product1=searchByProductName(product);
+		}
+		return product1;
+	}
+
+	//Searching product by Name
+	@Override
+	public ArrayList<Product> searchByProductName(String product)  {
+		ArrayList<Product> product1=new ArrayList<Product>();
+		 product1=prepo.findProductName(product);
+		return product1;
 	}
 
 }
