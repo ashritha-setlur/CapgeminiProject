f04e2b1984c03391b6462c1ec19a8f184ec23bc4
Ashritha Setlur <ashritha.setlur@capgemini.com>
2018-08-20 10:46:38 +0530

    Changes To wishlist Bean

--- CapStore_DB/src/main/java/com/capgemini/capstore/beans/Wishlist.java
+++ CapStore_DB/src/main/java/com/capgemini/capstore/beans/Wishlist.java
@@ -6,7 +6,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.OneToMany;
+import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 
 @Entity
@@ -17,7 +17,7 @@ public class Wishlist {
 	@GeneratedValue(strategy=GenerationType.AUTO)
 	private int wishlistId;
 
-	@OneToMany
+	@ManyToMany
 	private List<Product> products;
 
 	//Constructors
